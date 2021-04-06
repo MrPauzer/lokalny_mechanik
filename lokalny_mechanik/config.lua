926604e393ad48359e62697ebd62b685ddf90dba
Pauzer <63166329+MrPauzer@users.noreply.github.com>
2021-04-06 13:01:19 +0200

    Add files via upload

---
+++ lokalny_mechanik/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+Config.Locale = 'en' -- język en
+
+Config.RepairTime = 4000
+Config.EnableSoundEffect = true -- wpisz false jeżeli nie chcesz efektów dźwiękowych
+Config.Blips = true -- wpisz falsem, jeżeli chesz wyłączyć blipy
+
+-- Tutaj dajesz coordy gdzie chcesz mieć stacje naprawczą
+Config.Stations = {
+	{x = -36.52, y = -1088.55, z = 26.02, cost = 5000},
+	{x = -211.15, y = -1323.98, z = 30.49, cost = false},
+}
---
+++ lokalny_mechanik/locales/en.lua
@@ -0,0 +1,9 @@
+Locales['en'] = {
+	['press_repair_free'] = 'kliknij ~INPUT_PICKUP~ bezpłatnie naprawić pojazd',
+	['press_repair_cost'] = 'kliknij ~INPUT_PICKUP~ koszt ~g~$%s ~s~do naprawy pojazdu',
+	['repair_processing'] = 'Trwa naprawa, proszę czekać ...',
+	['repair_finish'] = '~g~Pojazd naprawiony!',
+	['cost_repair'] = 'Wydałeś ~g~$%s ~s~do naprawy pojazdu',
+	['not_enough_money'] = '~r~Nie masz pieniędzy!',
+	['blips_name'] = 'Lokalny Mechanik'
+}
